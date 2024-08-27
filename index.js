fd50655ff0c052499b49f81b354cb47ad6be76b0
Aditi Prabhakar <aditiprabhakar04@gmail.com>
2024-08-27 10:55:12 +0000

    few changes

---
+++ index.js
@@ -0,0 +1,35 @@
+// state is mainly used as an object that stores something.
+// var state = {
+//     taskList: [
+//         {
+//             imageUrl: "",
+//             taskType: "",
+//             taskTitle: "",
+//             taskDesc: ""
+//         },
+//         {
+//             imageUrl: "",
+//             taskType: "",
+//             taskTitle: "",
+//             taskDesc: ""
+//         },
+//         {
+//             imageUrl: "",
+//             taskType: "",
+//             taskTitle: "",
+//             taskDesc: ""
+//         },
+//         {
+//             imageUrl: "",
+//             taskType: "",
+//             taskTitle: "",
+//             taskDesc: ""
+//         },
+//         {
+//             imageUrl: "",
+//             taskType: "",
+//             taskTitle: "",
+//             taskDesc: ""
+//         },
+//     ]
+// }
