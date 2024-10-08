7c274b6e5a88253ff2c15338e3a6f1a0b8a051ff
Aditi Prabhakar <aditiprabhakar04@gmail.com>
2024-10-08 17:45:37 +0000

    nodemon and gitignore for it's modules and package-lock files

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//custom scripts in package.json ("aditi": "node server.js" can also run command but we have to write 'npm run aditi' instead of just 'npm start')
+//* custom scripts in package.json ("aditi": "node server.js" can also run command but we have to write 'npm run aditi' instead of just 'npm start')
 
-const http = require('http'); //third-party file storing of our local browser http to a variable http
+const http = require('http'); //^ third-party file storing of our local browser http to a variable http
+const port = 8081; //~ local port run
 
-const port = 8081;
-
-http.createServer((req, res) => {
-    res.writeHead(200, { "Content-type": "text/html" }); //200 is the http code for 'OK'
-    res.write("<h2>Hey server started :)</h2>")
+http.createServer((req, res) => {              //& callback func
+    res.writeHead(200, { "Content-type": "text/html" }); //~ 200 is the http code for 'OK'
+    res.write("<h2>Hey server started!! (nodemon included) :)</h2>")
     res.end();
 })
 
-    .listen(port, () => {
-        console.log(`NodeJs server started running on port ${port}`); //listens to http always on port(8081) is a callback function, for successful conformation 
+    .listen(port, () => {          //& callback func
+        console.log(`NodeJs server started running on port ${port}`); //~ listens to http always on port(8081) is a callback function, for successful conformation 
     })
