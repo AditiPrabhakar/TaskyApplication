930f8e12454a4a3e61c1efa09bd4fce6271bd00e
Aditi Prabhakar <aditiprabhakar04@gmail.com>
2024-10-08 18:04:11 +0000

    http methods

--- server.js
+++ server.js
@@ -3,10 +3,22 @@
 const http = require('http'); //^ third-party file storing of our local browser http to a variable http
 const port = 8081; //~ local port run
 
+const todoList = ["learn", "apply things", "succeed"];
+
 http.createServer((req, res) => {              //& callback func
-    res.writeHead(200, { "Content-type": "text/html" }); //~ 200 is the http code for 'OK'
-    res.write("<h2>Hey server started!! (nodemon included) :)</h2>")
+    const {method, url} = req
+    // console.log("method" + method, "\nurl" + url);
+    if(url === "/todos"){
+        if(method === "GET")
+        {
+            res.writeHead(200);
+            res.write(todoList.toString());
+        }
+    }
     res.end();
+    // res.writeHead(200, { "Content-type": "text/html" }); //~ 200 is the http code for 'OK'
+    // res.write("<h2>Hey server started!! (nodemon included) :)</h2>")
+    // res.end();
 })
 
     .listen(port, () => {          //& callback func
